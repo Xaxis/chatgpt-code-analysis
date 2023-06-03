fed6c5b64ffecb3beb3b7c02fc66019f6cf85450
Xaxis <396863+Xaxis@users.noreply.github.com>
2023-06-03 00:10:42 -0800

    Further improvements

--- index.py
+++ index.py
@@ -26,36 +26,33 @@ def download_github_repo(repo_url, local_dir="target_repos"):
     os.makedirs(full_local_path, exist_ok=True)
     Repo.clone_from(repo_url, full_local_path)
 
-def read_and_tokenize_all_files(local_dir="target_repos", ignore=None):
+def read_and_tokenize_all_files(repo_path, ignore=None):
     if ignore is None:
-        ignore = []  # list of patterns to ignore
-
+        ignore = []
     structured_tokens = []
-    for repo in os.listdir(local_dir):
-        repo_path = os.path.join(local_dir, repo)
-        for subdir, dirs, files in os.walk(repo_path):
-            dirs[:] = [d for d in dirs if not any(fnmatch.fnmatch(d, pattern) for pattern in ignore)]
-            for file in files:
-                if any(fnmatch.fnmatch(file, pattern) for pattern in ignore):
-                    continue
-                file_path = os.path.join(subdir, file)
+    for subdir, dirs, files in os.walk(repo_path):
+        dirs[:] = [d for d in dirs if not any(fnmatch.fnmatch(d, pattern) for pattern in ignore)]
+        for file in files:
+            if any(fnmatch.fnmatch(file, pattern) for pattern in ignore):
+                continue
+            file_path = os.path.join(subdir, file)
+            try:
+                lexer = get_lexer_for_filename(file_path)
+            except pygments.util.ClassNotFound:
                 try:
-                    lexer = get_lexer_for_filename(file_path)
-                except pygments.util.ClassNotFound:
-                    try:
-                        with open(file_path, 'r') as f:
-                            content = f.read()
-                        lexer = guess_lexer(content)
-                    except:
-                        continue
-                with open(file_path, 'r') as f:
-                    code = f.read()
-                    file_tokens = list(pygments.lex(code, lexer))
-                    structured_tokens.append({
-                        'repo': repo,
-                        'file_path': file_path,
-                        'tokens': file_tokens
-                    })
+                    with open(file_path, 'r') as f:
+                        content = f.read()
+                    lexer = guess_lexer(content)
+                except:
+                    continue
+            with open(file_path, 'r') as f:
+                code = f.read()
+                file_tokens = list(pygments.lex(code, lexer))
+                structured_tokens.append({
+                    'repo': repo_path.split('/')[-1],
+                    'file_path': file_path,
+                    'tokens': file_tokens
+                })
     return structured_tokens
 
 def ask_gpt4_about_code(structured_tokens, question, past_conversation="", max_tokens=1000):
@@ -74,8 +71,10 @@ def prompt_user():
         repo_url = input(colored("\n>> Enter GitHub repository URL [https://github.com/Xaxis/chatgpt-code-analysis]: ", "green"))
         repo_url = repo_url if repo_url else "https://github.com/Xaxis/chatgpt-code-analysis"
         download_github_repo(repo_url, './target_repos')
+        repo_name = repo_url.split('/')[-1]
+        repo_path = os.path.join('./target_repos', repo_name)
         code_tokens = read_and_tokenize_all_files(
-            './target_repos',
+            repo_path,
             ["*.txt", "temp", ".git", "*.iml", ".idea", ".gitignore", "*.md", "*.json", "*.yml", "*.yaml", "*.xml", "*.gradle", "*.properties"]
         )
 
